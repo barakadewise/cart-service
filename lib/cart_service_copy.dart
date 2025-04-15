957bdc9ab900136b0c05fd1c20934442912ab0ad
Baraka <barakadewise@gmail.com>
2025-04-15 20:15:20 +0300

    feat: Downgrade file_picker dependency to version 8.1.2; update related configurations in lock files and service implementation

--- lib/cart_service_copy.dart
+++ lib/cart_service_copy.dart
@@ -9,58 +9,40 @@ class CartServiceCopy<T, C extends CartBaseModel<T>> {
   }
 
   final List<C> _cartItems = [];
+  List<C> get cartItems => _cartItems;
 
+  /// Method to add a single item to the cart
+  /// if quantity not given default value for the product
+  /// will be int quantinty =1  exmple usage cartService.addItem(CartModel(product: product))
+  /// where [product] is the  product to added to cart.
+  /// sample usage with quantity given cartService.addItem(CartModel(product: product,quantity: 20))
   void addItem(C item) {
     final existingIndex = _cartItems.indexWhere((e) => e.isSameItemAs(item));
-
     if (existingIndex != -1) {
       final updated = _cartItems[existingIndex].mergeWith(item) as C;
       _cartItems[existingIndex] = updated;
-      debugPrint('Item updated in cart');
+      debugPrint(
+          ' updated in cart item to ${_cartItems[existingIndex].quantity}');
     } else {
       _cartItems.add(item);
-      debugPrint('Item added to cart');
+      debugPrint('${item.quantity} added to cart');
     }
   }
 
-  ///add multiple products to cart
-  // void addMultipleItems(List<DefaultCartModel<T>> items) {
-  //   for (var item in items) {
-  //     // Check if the product already exists in the cart
-  //     final existingItemIndex =
-  //         _cartItems.indexWhere((cartItem) => cartItem.product == item.product);
+  ///remove all related items (multiple products)
+  ///Use this in case you  need to specify spicific
+  ///array of cart items and not the whole cart items
+  ///for this method you have to specific Cartmodel<T>
+  ///where <T>  is you Productmodel (e.g CartModel<ProductModel>)
+  void removeItems(List<C> itemsToRemove) {
+    _cartItems.removeWhere((cartItem) {
+      return itemsToRemove.any((itemToRemove) {
+        return cartItem.product == itemToRemove.product;
+      });
+    });
+  }
 
-  //     if (existingItemIndex != -1) {
-  //       // If the product exists, increment its quantity
-  //       final existingItem = _cartItems[existingItemIndex];
-  //       _cartItems[existingItemIndex] = existingItem.copyWith(
-  //           quantity: existingItem.quantity + item.quantity);
-  //       debugPrint(' Product ${item.product} exist Incrementing quantity.');
-  //     } else {
-  //       // If the product doesn't exist, add it to the cart
-  //       _cartItems.add(item);
-  //       debugPrint('Added ${item.product} to the cart.');
-  //     }
-  //   }
-  // }
-
-  // /// get map list of products in cart with their quantities
-  // // List<Map<String, dynamic>> currentUserOrders(
-  // //     Map<String, dynamic> Function(T) toJson) {
-  // //   return _cartItems.map((item) => item.toJson(toJson)).toList();
-  // // }
-
-  // ///increment single  product on the cart
-  // void incrementItem(T item) {
-  //   final index = _cartItems.indexWhere((e) => e.product == item);
-
-  //   /// throw product not found if not in the list
-  //   assert(index != -1, 'Error: Product $item not found in the cart!');
-  //   if (index != -1) {
-  //     final current = _cartItems[index];
-  //     _cartItems[index] = current.copyWith(quantity: current.quantity + 1);
-  //   }
-  // }
+  ///decrement item  in the cart
   void incrementItem(C item) {
     final index = _cartItems.indexWhere((e) => e.isSameItemAs(item));
     if (index != -1) {
@@ -68,13 +50,21 @@ class CartServiceCopy<T, C extends CartBaseModel<T>> {
       _cartItems[index] = existing.copyWith(
         quantity: existing.quantity + 1,
       ) as C;
+      debugPrint(
+          "product incremented from ${existing.quantity} to ${_cartItems[index].quantity}");
     }
   }
 
+  /// Remove single product(single product)
+  /// Use this method only when you need to remove specific
+  /// product form the cartitems regardless its quantity
+  /// T is your specified productmodel example
+  /// cartService.removeItem(ProductModel product)
   void removeItem(C item) {
     _cartItems.removeWhere((e) => e.isSameItemAs(item));
   }
 
+  ///increment single  product on the cart
   void decrementItem(C item) {
     final index = _cartItems.indexWhere((e) => e.isSameItemAs(item));
     if (index != -1) {
@@ -88,132 +78,8 @@ class CartServiceCopy<T, C extends CartBaseModel<T>> {
     }
   }
 
-  // ///decrement item  in the cart
-  // void decrementItem(T item) {
-  //   final index = _cartItems.indexWhere((e) => e.product == item);
-
-  //   ///throw an error if the product was not found in the cart
-  //   assert(index != -1, 'Error: Product $item not found in the cart!');
-  //   if (index != -1) {
-  //     final current = _cartItems[index];
-
-  //     if (current.quantity > 1) {
-  //       _cartItems[index] = current.copyWith(quantity: current.quantity - 1);
-  //     } else {
-  //       _cartItems.removeAt(index);
-  //     }
-  //   }
-  // }
-
-  // /// Remove single product(single product)
-  /// Use this method only when you need to remove specific
-  /// product form the cartitems regardless its quantity
-  /// T is your specified productmodel example
-  /// cartService.removeItem(ProductModel product)
-  // void removeItem(T item) {
-  //   _cartItems.removeWhere((i) => i.product == item);
-  // }
-
-  ///remove all related items (multiple products)
-  ///Use this in case you  need to specify spicific
-  ///array of cart items and not the whole cart items
-  ///for this method you have to specific Cartmodel<T>
-  ///where <T>  is you Productmodel (e.g CartModel<ProductModel>)
-
-  // void removeItems(List<CartModel<T>> itemsToRemove) {
-  //   _cartItems.removeWhere((cartItem) {
-  //     return itemsToRemove.any((itemToRemove) {
-  //       return cartItem.product == itemToRemove.product;
-  //     });
-  //   });
-  // }
-
-  ///calculate the products total prcices
-  // double getTotalPrice(double Function(T product) priceExtractor) {
-  //   double total = 0.0;
-  //   for (var item in _cartItems) {
-  //     total += priceExtractor(item.product) * item.quantity;
-  //   }
-  //   return total;
-  // }
-
-  // /// Return all items in  cart
-  // List<DefaultCartModel<T>> getItems() {
-  //   return _cartItems;
-  // }
-
-  // ///method to clear all cart
-  // void clearCart() {
-  //   _cartItems.clear();
-  // }
-
-  ///return user carts list  from APi
-  // List<CartModel<T, D>> userOrder(
-  //     List<dynamic> data,
-  //     T Function(Map<String, dynamic>) fromJson,
-  //     D? Function(Map<String, dynamic>) detailsFromJson) {
-  //   List<CartModel<T, D>> list = [];
-  //   try {
-  //     for (var i in data) {
-  //       list.add(CartModel.fromJson(i, fromJson, detailsFromJson));
-  //     }
-  //     return list;
-  //   } catch (e, stack) {
-  //     debugPrint("Errror occured while adding data to cart:$stack");
-  //     rethrow;
-  //   }
-  // }
-
-  /// send cart order to server
-  /// http method is default post if no  method selected from [RequestEnum]
-  /// params are null by default  and token are null by default
-  // Future<Either<ErrorMap, List<T>>> fetchProducts({
-  //   RequestEnum method = RequestEnum.get,
-  //   Map<String, dynamic>? params,
-  //   String? token,
-  //   String? dataKey,
-  //   required T Function(Map<String, dynamic>) fromJson,
-  //   required String endPoint,
-  // }) async {
-  //   try {
-  //     debugPrint(
-  //         "📥 Sending GET request for endpoint: $endPoint with params: $params");
-  //     return await CartRepository<T>().productItems(
-  //         endPoint: endPoint,
-  //         token: token,
-  //         params: params,
-  //         fromJson: fromJson,
-  //         dataKey: dataKey);
-  //   } catch (e, stack) {
-  //     debugPrint("🔥 Exception in serverRequest: $e");
-  //     debugPrint("🧱 Stack Trace: $stack");
-  //     rethrow;
-  //   }
-  // }
-
-  /// send cart order to server
-  /// http method is default post if no  method selected from [RequestEnum]
-  /// params are null by default  and token are null by default
-  // Future<Either<ErrorMap, List<T>>> fetchUserOrder({
-  //   RequestEnum method = RequestEnum.get,
-  //   Map<String, dynamic>? params,
-  //   String? token,
-  //   String? dataKey,
-  //   required T Function(Map<String, dynamic>) fromJson,
-  //   required String endPoint,
-  // }) async {
-  //   try {
-  //     debugPrint(
-  //         "📥 Sending GET request for endpoint: $endPoint with params: $params");
-  //     return await CartRepository<T>().productItems(
-  //         endPoint: endPoint,
-  //         token: token,
-  //         params: params,
-  //         fromJson: fromJson,
-  //         dataKey: dataKey);
-  //   } catch (e, stack) {
-  //     debugPrint("🔥 Exception in serverRequest: $e \n Stack Trace: $stack");
-  //     rethrow;
-  //   }
-  // }
+  //Clear item in cart
+  void clearItems() {
+    _cartItems.clear();
+  }
 }
